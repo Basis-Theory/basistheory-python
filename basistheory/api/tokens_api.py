9071fad94f2cf1b984185854d59bd1229df3807d
Adam Letizia <adam@basistheory.com>
2022-01-12 18:34:05 -0600

    fix: add nested collection_format for token metadata query

--- basistheory/api/tokens_api.py
+++ basistheory/api/tokens_api.py
@@ -327,6 +327,7 @@ class TokensApi(object):
                 'collection_format_map': {
                     'id': 'multi',
                     'type': 'multi',
+                    'metadata': 'nested',
                 }
             },
             headers_map={
