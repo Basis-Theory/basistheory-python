e48163f22873ba512ef6e8945851e71b3bb2b04e
Lucas Chociay <lucas@basistheory.com>
2022-04-07 10:25:18 -0300

    feat: add get invitation by id method

--- basistheory/api/tenants_api.py
+++ basistheory/api/tenants_api.py
@@ -598,6 +598,58 @@ class TenantsApi(object):
             },
             api_client=api_client
         )
+        self.tenant_invitations_get_by_id_endpoint = _Endpoint(
+            settings={
+                'response_type': (TenantInvitationResponse,),
+                'auth': [
+                    'apiKey'
+                ],
+                'endpoint_path': '/tenants/self/invitations/{invitationId}',
+                'operation_id': 'tenant_invitations_get_by_id',
+                'http_method': 'GET',
+                'servers': None,
+            },
+            params_map={
+                'all': [
+                    'invitation_id',
+                    'request_options'
+                ],
+                'required': [
+                    'invitation_id',
+                ],
+                'nullable': [
+                ],
+                'enum': [
+                ],
+                'validation': [
+                ]
+            },
+            root_map={
+                'validations': {
+                },
+                'allowed_values': {
+                },
+                'openapi_types': {
+                    'invitation_id':
+                        (str,),
+                },
+                'attribute_map': {
+                    'invitation_id': 'invitationId',
+                },
+                'location_map': {
+                    'invitation_id': 'path',
+                },
+                'collection_format_map': {
+                }
+            },
+            headers_map={
+                'accept': [
+                    'application/json'
+                ],
+                'content_type': [],
+            },
+            api_client=api_client
+        )
 
     def delete(
         self,
@@ -1482,3 +1534,84 @@ class TenantsApi(object):
         kwargs['_host_index'] = kwargs.get('_host_index')
         kwargs['_request_auths'] = kwargs.get('_request_auths', None)
         return self.tenant_members_get_endpoint.call_with_http_info(**kwargs)
+
+    def get_invitation_by_id(
+        self,
+        invitation_id,
+        **kwargs
+    ):
+        """get_invitation_by_id  # noqa: E501
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+
+        >>> thread = api.get_invitation_by_id(invitation_id, async_req=True)
+        >>> result = thread.get()
+
+        Args:
+            invitation_id (str):
+
+        Keyword Args:
+            _return_http_data_only (bool): response data without head status
+                code and headers. Default is True.
+            _preload_content (bool): if False, the urllib3.HTTPResponse object
+                will be returned without reading/decoding response data.
+                Default is True.
+            _request_timeout (int/float/tuple): timeout setting for this request. If
+                one number provided, it will be total request timeout. It can also
+                be a pair (tuple) of (connection, read) timeouts.
+                Default is None.
+            _check_input_type (bool): specifies if type checking
+                should be done one the data sent to the server.
+                Default is True.
+            _check_return_type (bool): specifies if type checking
+                should be done one the data received from the server.
+                Default is True.
+            _spec_property_naming (bool): True if the variable names in the input data
+                are serialized names, as specified in the OpenAPI document.
+                False if the variable names in the input data
+                are pythonic names, e.g. snake case (default)
+            _content_type (str/None): force body content-type.
+                Default is None and content-type will be predicted by allowed
+                content-types and body.
+            _host_index (int/None): specifies the index of the server
+                that we want to use.
+                Default is read from the configuration.
+            async_req (bool): execute request asynchronously
+
+        Returns:
+            TenantInvitationResponse
+                If the method is called asynchronously, returns the request
+                thread.
+        """
+        if kwargs.get('request_options'):
+            set_request_options(kwargs.pop('request_options'), self)
+
+        kwargs['async_req'] = kwargs.get(
+            'async_req', False
+        )
+        kwargs['_return_http_data_only'] = kwargs.get(
+            '_return_http_data_only', True
+        )
+        kwargs['_preload_content'] = kwargs.get(
+            '_preload_content', True
+        )
+        kwargs['_request_timeout'] = kwargs.get(
+            '_request_timeout', None
+        )
+        kwargs['_check_input_type'] = kwargs.get(
+            '_check_input_type', True
+        )
+        kwargs['_check_return_type'] = kwargs.get(
+            '_check_return_type', True
+        )
+        kwargs['_spec_property_naming'] = kwargs.get(
+            '_spec_property_naming', False
+        )
+        kwargs['_content_type'] = kwargs.get(
+            '_content_type')
+        kwargs['_host_index'] = kwargs.get('_host_index')
+        kwargs['invitation_id'] = \
+            invitation_id
+        kwargs['_request_auths'] = kwargs.get('_request_auths', None)
+        return self.tenant_invitations_get_by_id_endpoint.call_with_http_info(**kwargs)
